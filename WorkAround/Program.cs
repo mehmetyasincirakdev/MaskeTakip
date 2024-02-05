a009c084a7daee6f7d277f25b886dd3e37f1254d
myasincirak <myasincirak@DESKTOP-OT4U6F9>
2024-02-05 15:25:07 +0300

    Add project files.

---
+++ WorkAround/Program.cs
@@ -0,0 +1,13 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+PttManager pttManager = new PttManager(new PersonManager());
+PttManager foreignerPttManager = new PttManager(new ForeignerManager());
+
+Person person1 = new Person();
+person1.FirstName = "Mehmet Yasin";
+person1.LastName = "Çirak";
+person1.DateOfBirthYear = 1996;
+person1.NationalIdentity = 25;
+
+pttManager.GiveMask(person1);
